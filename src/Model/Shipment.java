98c5714b5239e09966015e0d849ee95e3fa3ee8c
mariathomollari <maria.thomollari@fti.edu.al>
2025-12-16 21:50:22 +0100

    test

--- src/Model/Shipment.java
+++ src/Model/Shipment.java
@@ -17,5 +17,31 @@ public class Shipment {
     public void createShipment() {
         this.shippedAt = new Date();
         this.status = "Created";
+        if (this.trackingNumber == null || this.trackingNumber.isEmpty()) {
+            generateTrackingNumber();
+        }
+        shipmentDB.put(this.shipmentId, this);
+    }
+
+    public void updateDetails(String courier) {
+        this.courier = courier;
+        updateStatus("Updated");
+    }
+
+    public void deleteShipment() {
+        shipmentDB.remove(this.shipmentId);
+    }
+
+    public void generateTrackingNumber() {
+        this.trackingNumber = "TRK" + (1000 + this.shipmentId);
+    }
+
+    public void updateStatus(String newStatus) {
+        this.status = newStatus;
+    }
+
+    public void getTrackingInfo() {
+        System.out.println("Tracking Number: " + this.trackingNumber);
+        System.out.println("Status: " + this.status);
     }
 }
