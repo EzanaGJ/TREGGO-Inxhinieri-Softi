43af0722da87d7f7961ac7fe66458629fe37ccfa
mariathomollari <maria.thomollari@fti.edu.al>
2025-12-21 22:07:21 +0100

    test

--- src/Model/Messages.java
+++ src/Model/Messages.java
@@ -1,58 +1,36 @@
 package Model;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class Messages {
-    int messageId;
-    public int senderId;
-    public int receiverId;
-    public String content;
-    Date timestamp;
-    public boolean read;
 
-    public static List<Messages> messageDatabase = new ArrayList<>();
-    public static int idCounter = 1;
+    private int messageId;
+    private int senderId;
+    private int receiverId;
+    private String content;
+    private Date timestamp;
+    private boolean read;
 
-    public Messages(int senderId, int receiverId, String content) {
-        this.messageId = idCounter++;
+    // Constructor used by DAO
+    public Messages(int messageId, int senderId, int receiverId,
+                    String content, Date timestamp, boolean read) {
+        this.messageId = messageId;
         this.senderId = senderId;
         this.receiverId = receiverId;
         this.content = content;
-        this.timestamp = new Date();
-        this.read = false;
+        this.timestamp = timestamp;
+        this.read = read;
     }
 
-    public void sendMessage() {
-        messageDatabase.add(this);
-        System.out.println("Message sent from user " + senderId + " to user " + receiverId + ": " + content);
-    }
+    // Getters
+    public int getMessageId() { return messageId; }
+    public int getSenderId() { return senderId; }
+    public int getReceiverId() { return receiverId; }
+    public String getContent() { return content; }
+    public Date getTimestamp() { return timestamp; }
+    public boolean isRead() { return read; }
 
-    public static List<Messages> getMessage(int userId) {
-        List<Messages> userMessages = new ArrayList<>();
-        for (Messages m : messageDatabase) {
-            if (m.receiverId == userId) {
-                userMessages.add(m);
-            }
-        }
-        return userMessages;
-    }
-
-    public void markAsRead() {
-        this.read = true;
-        System.out.println("Message " + messageId + " marked as read.");
-    }
-
-    public void deleteMessage() {
-        messageDatabase.removeIf(m -> m.messageId == this.messageId);
-        System.out.println("Message " + messageId + " deleted.");
-    }
-
-    static void listMessages(int userId) {
-        System.out.println("Messages for user " + userId + ":");
-        for (Messages m : getMessage(userId)) {
-            System.out.println(" - From: " + m.senderId + ", Content: " + m.content + ", Read: " + m.read + ", Time: " + m.timestamp);
-        }
-    }
+    // Setters
+    public void setContent(String content) { this.content = content; }
+    public void setRead(boolean read) { this.read = read; }
 }
